af879356c84858e6b609273dab0cd367c9a335d9
Vaelico333 <darzoga@gmail.com>
2022-06-24 03:29:46 +0100

    Corrección

--- Python/prod_cereal_cos_40_50_60_65_70_71.py
+++ Python/prod_cereal_cos_40_50_60_65_70_71.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-filepath = '../../sample_data/02 Introduction to Pandas/edited_googleplaystore_dataset.csv'
 cos_brut_df = pd.read_csv('..\CSV_files\produccion_bruta_cosechas_1940_1950_1960_1965_1970_1971.csv', index_col='Fechas')
 cos_cereal_df = cos_brut_df[['CultivosCereales','Trigo','Centeno','MaizGrano','Cebada','Avena','Mijo','TrigoSarraceno','Arroz']]
 print(cos_cereal_df)
--- Python/prod_heno_cos_40_50_60_65_70_71.py
+++ Python/prod_heno_cos_40_50_60_65_70_71.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-filepath = '../../sample_data/02 Introduction to Pandas/edited_googleplaystore_dataset.csv'
 cos_brut_df = pd.read_csv('..\CSV_files\produccion_bruta_cosechas_1940_1950_1960_1965_1970_1971.csv', index_col='Fechas')
 cos_cereal_df = cos_brut_df[['Heno_ForrajeVerdeHeno_Total','HenoHierbaPerenne','HenoHierbaAnual','HenoNatural']]
 print(cos_cereal_df)
