3da5c38e9773eac0ede26b94754b91a17af59260
Vaelico333 <darzoga@gmail.com>
2022-06-24 03:28:47 +0100

    Correción

--- Python/prod_brut_cos_40_50_60_65_70_71.py
+++ Python/prod_brut_cos_40_50_60_65_70_71.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-filepath = '../../sample_data/02 Introduction to Pandas/edited_googleplaystore_dataset.csv'
 cos_brut_df = pd.read_csv('..\CSV_files\produccion_bruta_cosechas_1940_1950_1960_1965_1970_1971.csv', index_col=['Fechas'])
 print(cos_brut_df)
 cos_brut_df.plot(style='.-', title='Vista general de la prod. bruta 1940-1971',ylabel='Miles de toneladas',xlabel='Año')
